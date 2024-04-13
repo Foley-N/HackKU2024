48992db3c8feaa9062cfaa7a728dc18cf060a6ed
Tanner Collins <72166757+TannerCollins309@users.noreply.github.com>
2024-04-13 10:28:49 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
 import pandas as pd
 import os
+import platform
 
-df = pd.read_csv(os.getcwd() + "tweets/tweets_labelled_09042020_16072020.csv")
+def import_csv():
+    OS = platform.system()
+    
+    if OS == "Linux":
+        df = pd.read_csv(os.getcwd() + "/tweets/tweets_labelled_09042020_16072020.csv", sep=";")
+    elif OS == "Windows":
+        pass
+    else:
+        print("nope")
+    
+    return df
+
+
+
+df = import_csv()
