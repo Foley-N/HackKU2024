673a9631327c8331329f25f6790b6b69ee439b3d
Tanner Collins <72166757+TannerCollins309@users.noreply.github.com>
2024-04-13 11:51:40 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,25 +2,25 @@ import pandas as pd
 import os
 import platform
 import vaderSentiment as vs
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 
 def import_csv():
     OS = platform.system()
-    
+
     if OS == "Linux":
         df = pd.read_csv(os.getcwd() + "/tweets/tweets_labelled_09042020_16072020.csv", sep=";")
     elif OS == "Windows":
         df = pd.read_csv(os.getcwd() + "\\tweets\\tweets_labelled_09042020_16072020.csv", sep=";")
     else:
         print("nope")
-    
+
     return df
 
 
 df = import_csv()
 
 # print(df.head())
-
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 sentences = df['text'].tolist()
 
