e459e28116f68e3894a2c025d1d0f690f430da44
Noah Foley <noahfoleypro@gmail.com>
2024-04-13 10:29:46 -0500

    testing

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import pandas as pd
 import os
 
-df = pd.read_csv(os.getcwd() + "tweets/tweets_labelled_09042020_16072020.csv")
+df = pd.read_csv(os.getcwd() + "\\tweets\\tweets_labelled_09042020_16072020.csv", sep=";")
+
+print(df.head())
