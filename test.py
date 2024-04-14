45697f59d0342bc2af4549d6cdd7c2c9cb255839
Noah Foley <noahfoleypro@gmail.com>
2024-04-13 21:46:20 -0500

    edited filterbydate

--- test.py
+++ test.py
@@ -29,18 +29,16 @@ def searchDF(df, list, wordList):
     bools = pd.Series(mask)
     return df[bools.values]
 
-def filterByDate(df, list, dateList):
-    df.sort_values(by='created_at', inplace=True)
-    mask = map((lambda x : search(x, dateList)), list)
-    bools = pd.Series(mask)
-    return df[bool.values]
-
 def dateRangeDist(sYear,sMonth,sDay, eYear, eMonth, eDay):
   start = datetime.date(sYear, sMonth, sDay)
   end = datetime.date(eYear, eMonth, eDay)
   date_list = [f"{date.year}-{date.month}-{date.day}" for date in (start + datetime.timedelta(days=x) for x in range((end-start).days + 1))]
   return date_list
 
+def filter_by_dates(df, dates):
+    df['created_at'] = pd.to_datetime(df['created_at'])
+    mask = df['created_at'].isin(dates)
+    return df[mask]
 
 df = import_csv()
 
@@ -51,8 +49,8 @@ amazonTweets = searchDF(df, sentences, ['$AMZN']).sort_values(by='created_at', a
 newDF = searchDF(df, sentences, ['$AMZN'])
 
 dates = (dateRangeDist(2020, 4, 15, 2020, 4, 30))
-
-print(filterByDate(newDF, dates, ['$AMZN']))
+print(dates)
+print(filter_by_dates(newDF, dates))
 
 #amazonText = amazonTweets['text'].tolist()
 
@@ -62,7 +60,7 @@ print(filterByDate(newDF, dates, ['$AMZN']))
 print(sentences[115])
 print(sentence)"""
 
-sentimentList = []
+"""sentimentList = []
 
 for sentence in amazonText:
     vs = analyzer.polarity_scores(sentence)
@@ -72,4 +70,4 @@ for sentence in amazonText:
 
 negativeSentiment = (sentiment['compound'] for sentiment in sentimentList if sentiment['compound'] < 0)
 positiveSentiment = (sentiment['compound'] for sentiment in sentimentList if sentiment['compound'] > 0)
-
+"""
