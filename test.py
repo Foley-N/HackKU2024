cb678235e090fc6b5f35f3bd680d988933ebc691
Noah Foley <noahfoleypro@gmail.com>
2024-04-13 19:58:03 -0500

    Hopefully fixed sortByDate

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import platform
+import datetime
 import vaderSentiment as vs
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
@@ -28,19 +29,18 @@ def searchDF(df, list, wordList):
     bools = pd.Series(mask)
     return df[bools.values]
 
-def filterByDate(df, dateList):
+def filterByDate(df, list, dateList):
     df.sort_values(by='created_at', inplace=True)
     mask = map((lambda x : search(x, dateList)), list)
     bools = pd.Series(mask)
     return df[bool.values]
-
-import datetime
 
 def dateRangeDist(sYear,sMonth,sDay, eYear, eMonth, eDay):
   start = datetime.date(sYear, sMonth, sDay)
   end = datetime.date(eYear, eMonth, eDay)
   date_list = [f"{date.year}-{date.month}-{date.day}" for date in (start + datetime.timedelta(days=x) for x in range((end-start).days + 1))]
   return date_list
+
 
 df = import_csv()
 
@@ -49,9 +49,9 @@ sentences = df['text'].tolist()
 amazonTweets = searchDF(df, sentences, ['$AMZN']).sort_values(by='created_at', ascending=True, na_position='first')
 newDF = searchDF(df, sentences, ['$AMZN'])
 
-print(dateRangeDist(2020, 4, 15, 2020, 7, 15))
+dates = (dateRangeDist(2020, 4, 15, 2020, 4, 30))
 
-print(filterByDate(newDF, 4))
+print(filterByDate(newDF, dates, ['$AMZN']))
 
 amazonText = amazonTweets['text'].tolist()
 
